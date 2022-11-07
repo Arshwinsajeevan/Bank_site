65fd8ffed8510387cab0025e90e9f1729aec598f
Arshwinsajeevan <arshwin619@gmail.com>
2022-11-07 13:14:22 +0530

    templete reference variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,10 +14,10 @@ export class LoginComponent implements OnInit {
   pswd:any
 
   userDetails:any={
-    1000:{acno:1000,username:"Arshi",password:"Arshi",balance:0},
-    1001:{acno:1001,username:"Abhi",password:"Abhi",balance:0},
-    1002:{acno:1002,username:"Sachi",password:"Sachi",balance:0},
-    1003:{acno:1003,username:"Arya",password:"Arya",balance:0}
+    1000:{acno:1000,username:"Arshi",password:"1000",balance:0},
+    1001:{acno:1001,username:"Abhi",password:"1001",balance:0},
+    1002:{acno:1002,username:"Sachi",password:"1002",balance:0},
+    1003:{acno:1003,username:"Arya",password:"1003",balance:0}
   }
 
   constructor() { }
@@ -29,9 +29,27 @@ export class LoginComponent implements OnInit {
   //   alert("Login clicked")               //event binding     (event name) = "methodname()"
   // }
 
-  login(){
-    var acno=this.acno      //short form
-    var pswd=this.pswd
+  // login(){
+  //   var acno=this.acno      //short form
+  //   var pswd=this.pswd
+
+  //   if(acno in this.userDetails){
+  //     if(pswd==this.userDetails[acno]['password']){
+  //         alert('Login success')
+  //     }
+  //     else{
+  //       alert('Incorrect password')
+  //     }
+  //   }
+  //   else{
+  //     alert('User not exist')
+  //   }
+
+  // }
+
+  login(a:any,b:any){
+    var acno=a.value     //short form
+    var pswd=b.value
 
     if(acno in this.userDetails){
       if(pswd==this.userDetails[acno]['password']){
@@ -47,15 +65,15 @@ export class LoginComponent implements OnInit {
 
   }
 
-  acnoChange(event:any){
-    this.acno=event.target.value
-    // console.log(event.target.value)
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  //   // console.log(event.target.value)
     
-  }
+  // }
 
-  pswdChange(event:any){
-     this.pswd=event.target.value
-    //  console.log(event.target.value);
-  }
+  // pswdChange(event:any){
+  //    this.pswd=event.target.value
+  //   //  console.log(event.target.value);
+  // }
 
 }
